449075009904d219f0c271b1349f124dd2a9092b
Daniel Papert <dpapert@gmail.com>
2022-08-18 19:04:23 -0400

    Reorganized files so high score appears on different page, different sections of javascript. Added questions.

---
+++ assets/js/questions.js
@@ -0,0 +1,71 @@
+// Questions (many taken from W3School quizes!)
+
+var questions = [
+    {
+        question: "Which of the following data types cannot be modified?",
+        choices: ["strings", "variables", "constants", "booleans"],
+        answer: "constants"
+    },
+    {
+        question: "What coding language is used to style webpages?",
+        choices: ["css", "javascript", "html", "python"],
+        answer: "css"
+    },
+    {
+        question: "An external Javascript file must contain the <script> tag.",
+        choices: ["True, False"],
+        answer: "False"
+    },
+    {
+        question: "Which event occurs when the user clicks on an HTML element?",
+        choices: ["onmouseover", "onmouseclick", "onclick", "onchange"],
+        answer: "onclick"
+    },
+    {
+        question: "How do you insert a comment in a CSS file?",
+        choices: ["// this is a comment //", "' this is a comment", "/* this is a comment */  ", "// this is a comment"],
+        answer: "/* this is a comment */  "
+    },
+
+    {
+        question: "Which CSS property controls text size?",
+        choices: ["font-size", "text-style", "text-size", "font-style"],
+        answer: "font-size"
+    },
+
+    {
+        question: "What is the default value of the position property?",
+        choices: ["fixed", "static", "absolute", "relative"],
+        answer: "static"
+    },
+
+    {
+        question: "How do you make each word in a text start with a captial letter?",
+        choices: ["text-transform:capitalize", "transform:capitalize", "text-style:capitalize", "None of the above"],
+        answer: "text-transform:capitalize"
+    },
+
+    {
+        question: "Which html tag would you use to make a bulleted list?",
+        choices: ["<ol>", "<bl>", "<list>", "<ul>"],
+        answer: "<ul>"
+    },
+
+    {
+        question: "Block elements are normally displayed without starting a new line.",
+        choices: ["True", "False"],
+        answer: "False"
+    },
+
+    {
+        question: "In HTML, which attribute is used to specify that an input field must be filled out?",
+        choices: ["placeholder", "formvalidate", "required", "validate"],
+        answer: "required"
+    },
+
+    {
+        question: "In HTML, which attribute is used to specify that an input field must be filled out?",
+        choices: ["placeholder", "formvalidate", "required", "validate"],
+        answer: "required"
+    }
+]
---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// Main Quiz Script
+
+// Time, Score, and Quiz Index Variables
+var quizIndex = 0;
+var score = 0; 
+var timer = 99;
+
+var startButton = document.querySelector("#start-quiz");
+var timeDisplay = document.querySelector("#timer");
+var scoreDisplay = document.querySelector("#score");
