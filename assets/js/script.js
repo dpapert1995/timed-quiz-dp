450671969307658bc38719980c9db6fb2c4fe13e
Daniel Papert <dpapert@gmail.com>
2022-08-18 20:46:01 -0400

    Added function to generate list of five random, non-repeating questions from list of twelve questions.

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,25 @@ var timer = 99;
 var startButton = document.querySelector("#start-quiz");
 var timeDisplay = document.querySelector("#timer");
 var scoreDisplay = document.querySelector("#score");
+var form = document.querySelector("#form");
+
+var quizLength = 5;
+var totalQuestions = questions.length + 1;
+var quizQuestions = getQuestions();
+
+// Generate an array of length quizLength, filled with random question indices from variable questions.
+function getQuestions() {
+let questionsAsked = [];
+let tempArray = questions;
+for (let i = 0; i < quizLength; i++) {
+    let j = Math.floor(Math.random() * tempArray.length);
+    questionsAsked[i] = tempArray[j];
+    // Remove question selected from question pool so quiz contains no repeating questions.
+    tempArray.splice(j,1);
+}
+return questionsAsked;
+}
+
+function endQuiz {
+
+}
